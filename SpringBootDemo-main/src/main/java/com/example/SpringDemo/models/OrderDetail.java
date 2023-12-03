3cebffe3deb98394d67c8fd78a8e677db3b1bbe0
DimLime <bacbobadao@gmail.com>
2023-12-03 12:42:28 +0700

    tesst

--- SpringBootDemo-main/src/main/java/com/example/SpringDemo/models/OrderDetail.java
+++ SpringBootDemo-main/src/main/java/com/example/SpringDemo/models/OrderDetail.java
@@ -1,7 +1,10 @@
 package com.example.SpringDemo.models;
 
+import jakarta.persistence.ManyToMany;
+
 public class OrderDetail {
-	
-	
+	private Long idoderdetail;
+	@ManyToMany
+	private Order idoder;
 	
 }
