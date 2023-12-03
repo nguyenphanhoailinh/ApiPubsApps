c820304e22fd9927ae2f48eba9642b073afa896a
DimLime <bacbobadao@gmail.com>
2023-12-03 22:21:35 +0700

    test OrderDetail

---
+++ SpringBootDemo-main/src/main/java/com/example/SpringDemo/repositories/OrderDetailRepository.java
@@ -0,0 +1,9 @@
+package com.example.SpringDemo.repositories;
+
+import com.example.SpringDemo.models.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+}
